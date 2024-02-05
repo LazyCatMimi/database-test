39dbc5ea16269d3838a5362e690d2c49f3e323c0
Quynh Vo <quynh.vo3314@gmail.com>
2024-02-05 14:15:43 -0500

    stuff

--- Backend/index.js
+++ Backend/index.js
@@ -1,41 +1,37 @@
-import {MongoClient, ServerApiVersion} from 'mongodb';
+import { MongoClient, ServerApiVersion } from "mongodb";
+import express from "express";
 const app = express();
 const PORT = 3001;
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-const uri = "mongodb+srv://quynh:hDTK0Nbt22pvFMjG@atlascluster.1rybup4.mongodb.net/react_db?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://quynh:hDTK0Nbt22pvFMjG@atlascluster.1rybup4.mongodb.net/react_db?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  // Connect the client to the server	(optional starting in v4.7)
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
 }
 run().catch(console.dir);
-
 
 async function connectAndQuery() {
   try {
     await client.connect();
-    const database = client.db('sample_analytics');
-    const collection = database.collection('accounts');
+    const database = client.db("sample_analytics");
+    const collection = database.collection("accounts");
 
     const result = await collection.findOne({ account_id: 198100 });
     console.log(result);
@@ -45,3 +41,17 @@ async function connectAndQuery() {
 }
 
 connectAndQuery();
+
+app.get("/", async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db("sample_analytics");
+    const collection = database.collection("accounts");
+
+    const result = await collection.findOne({ account_id: 198100 });
+    res.json(result);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
--- your-app-name/src/App.js
+++ your-app-name/src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/");
+        const jsonData = await response.json();
+
+        setData(jsonData);
+        console.log(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p>{data.id}</p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
